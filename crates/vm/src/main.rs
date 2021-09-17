14ebed3393043489e374623f99af5e067ec50f48
Stuart Haidon <serayen.sh@gmail.com>
2021-09-17 07:19:05 +0100

    Chapter 16.

--- crates/vm/src/main.rs
+++ crates/vm/src/main.rs
@@ -2,8 +2,20 @@ mod chunk;
 mod value;
 mod vm;
 
-use color_eyre::Result;
-use rlox::source_file::{SourceFile, SourceLocation};
+use codespan_reporting::{
+    diagnostic::Diagnostic,
+    term::termcolor::{ColorChoice, StandardStream},
+};
+use color_eyre::{
+    eyre::{eyre, Context},
+    Result,
+};
+use lasso::Rodeo;
+use rlox::{
+    lexer::Lexer,
+    source_file::{FileId, SourceFile, SourceLocation},
+    DiagnosticEmitter,
+};
 
 use chunk::{Chunk, OpCode};
 use value::Value;
@@ -16,70 +28,60 @@ fn main() -> Result<()> {
         .filter(|(key, _)| key == "LOX_TRACE")
         .count()
         > 0;
-    let mut vm = Vm::new(trace);
 
-    let mut source_file = SourceFile::new();
-    let id = source_file.add("script.lox", "return 1.2 + 3.4;");
-
-    let mut chunk = Chunk::default();
-    let a_id = chunk.add_constant(Value::Number(1.2));
-    let b_id = chunk.add_constant(Value::Number(3.4));
-    chunk.write(
-        OpCode::Constant,
-        SourceLocation {
-            file_id: id,
-            source_start: 7,
-            len: 3,
-        },
-    );
-    chunk.write(
-        a_id,
-        SourceLocation {
-            file_id: id,
-            source_start: 7,
-            len: 3,
-        },
-    );
-    chunk.write(
-        OpCode::Constant,
-        SourceLocation {
-            file_id: id,
-            source_start: 14,
-            len: 3,
-        },
-    );
-    chunk.write(
-        b_id,
-        SourceLocation {
-            file_id: id,
-            source_start: 14,
-            len: 3,
-        },
-    );
-    chunk.write(
-        OpCode::Add,
-        SourceLocation {
-            file_id: id,
-            source_start: 12,
-            len: 1,
-        },
-    );
-    chunk.write(
-        OpCode::Return,
-        SourceLocation {
-            file_id: id,
-            source_start: 0,
-            len: 6,
-        },
-    );
-
-    if trace {
-        eprintln!("Chunk trace");
-        chunk.disassemble(&source_file);
+    let args: Vec<_> = std::env::args().skip(1).collect();
+    if args.is_empty() {
+        eprintln!("Usage: rloxum [scripts]");
+        std::process::exit(64);
     }
 
-    eprintln!("\nExecution");
-    vm.interpret(&chunk, &source_file)?;
+    let stderr = StandardStream::stderr(ColorChoice::Always);
+    let mut emitter = DiagnosticEmitter::new(&stderr);
+    let mut interner = Rodeo::default();
+    // These three are used when instantiating a class.
+    interner.get_or_intern_static("this");
+    interner.get_or_intern_static("init");
+    interner.get_or_intern_static("super");
+    let mut vm = Vm::new(trace);
+
+    for file in args {
+        run_file(&file, &mut emitter, &mut vm, &mut interner)?;
+    }
 
     Ok(())
+}
+
+fn run_file(
+    path: &str,
+    emitter: &mut DiagnosticEmitter<'_>,
+    vm: &mut Vm,
+    interner: &mut Rodeo,
+) -> Result<()> {
+    let contents =
+        std::fs::read_to_string(path).with_context(|| eyre!("Failed to read file: '{}'", path))?;
+
+    let file_id = emitter.add_file(path, &contents);
+
+    if let Some(diags) = run(&contents, emitter, vm, interner, file_id)? {
+        emitter.emit_diagnostics(&diags)?;
+        std::process::exit(64);
+    }
+
+    Ok(())
+}
+
+fn run(
+    source: &str,
+    emitter: &mut DiagnosticEmitter<'_>,
+    vm: &mut Vm,
+    interner: &mut Rodeo,
+    file_id: FileId,
+) -> Result<Option<Vec<Diagnostic<FileId>>>> {
+    let scan_result = Lexer::scan_tokens(source, interner, file_id);
+    let tokens = match scan_result {
+        Ok(tokens) => tokens,
+        Err(diags) => return Ok(Some(diags)),
+    };
+
+    Ok(None)
 }
